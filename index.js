44950e4a2ea433b995f9ed457a90f7d06bc01771
SamuelWade <samuelrcwade@gmail.com>
2023-02-25 11:57:17 -0500

    Test Successful

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+const multiply = num1*num2;
+
+let random = Math.ceil(Math.random(0,1));
+
+let num3 = 28;
+
+let num4 = 6;
+
+const mod = num3%num4;
+
+const max = Math.max(20,1);
